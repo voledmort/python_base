dae8d61ae521f64c7e8ef7bd15396dd12ef232b7
aoao <ghiuznv@126.com>
2020-04-19 15:02:29 +0800

    测试代码

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-import this
-
 lista = [1, 2, 4]
-lista.pop(1)
+# lista.pop(1)
 
 lista.append(3)
-print(lista)
+print(lista[-1])
+#
+# for value in range(1, 5, 2):
+#     print(value)
+
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[:])
